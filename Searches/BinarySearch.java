39c6c35d91dfd1c56b56988172e9859defc8f859
Eugene <45047292+meowpeow@users.noreply.github.com>
2019-08-14 17:17:05 +0300

    Update BinarySearch.java

--- Searches/BinarySearch.java
+++ Searches/BinarySearch.java
@@ -1,5 +1,14 @@
-package algorithms.search;
+package algorithms.searches;
 
+/**
+ * Binary search is more effective way of search in comparison to linear search, where program just iterate through ALL elements.
+ * Instead, binary search can find element in sorted array in less amount of cycles, by using MIDDLE of the list.
+ * 1.      So to get it to work you need to assign to variables BEGIN of the list and END of it.
+ * 2.      Program now will find a middle of that list, and compare that MID variable to element we are searching for.
+ * 3.1.    If MID is BIGGER THAN X, than X is between BEGIN and MID
+ * 3.2.    Else if MID is LESS THAN X, than X is between MID and LAST
+ * 3.3     Repeat till mid == X
+ */
 public class BinarySearch {
 
     static void binarySearch(int arr[], int x) {
